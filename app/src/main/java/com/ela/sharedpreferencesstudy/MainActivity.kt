146da1be5ee4bd4b725ffa5419361f8b1ca8b3e3
Ale TI <alessandrodev@outlook.com>
2023-08-26 11:02:11 -0300

    commit shared

--- app/src/main/java/com/ela/sharedpreferencesstudy/MainActivity.kt
+++ app/src/main/java/com/ela/sharedpreferencesstudy/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.ela.sharedpreferencesstudy
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ela.sharedpreferencesstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var cor = ""
+
+    companion object{
+        const val ARQUIVO_PREFERENCIAS = "ArquivoPreferencias"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cor1.setOnClickListener(){
+            cor = "#2E7D32"
+            salvar(cor)
+        }
+        binding.cor2.setOnClickListener(){
+            cor = "#C62828"
+            salvar(cor)
+        }
+        binding.cor3.setOnClickListener(){
+            cor = "#F9A825"
+            salvar(cor)
+        }
+        binding.cor4.setOnClickListener(){
+            cor = "#1565C0"
+            salvar(cor)
+        }
+    }
+    private fun salvar(cor:String){
+        binding.btTrocarCorFundo.setOnClickListener(){
+            binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+            val preferencias = getSharedPreferences(ARQUIVO_PREFERENCIAS, MODE_PRIVATE)
+            val editor = preferencias.edit()
+            editor.putString("cor", cor)
+            editor.apply()
+        }
     }
 }
